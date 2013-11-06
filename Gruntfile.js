88ed6c73a32e150c0354c9af8a039622ba6f9c51
Robb Schiller <robb.schiller@gmail.com>
2013-11-06 15:19:52 -0500

    grunt

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    // Start writing tasks
+  });
+
+  // Load the installed tasks
+
+  // Default task
+  grunt.registerTask('default', ['']);
+
+};
