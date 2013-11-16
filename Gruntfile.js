a8cbc15286bc57502dca7d45b802178630f825d9
Robb Schiller <robb.schiller@gmail.com>
2013-11-16 11:49:35 -0500

    setup start

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,39 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    // Start writing tasks
+
+    watch: {
+      sass: {
+        files: ['public/sass/partials/**.scss', 'public/sass/modules/**.scss'],
+        tasks: 'sass:dev'
+      }
+    },
+
+    sass: {
+      dist: {
+        options: {
+          outputStyle: 'compressed'
+        },
+        files: {
+          'public/css/main.css': 'public/sass/main.scss'
+        }
+      },
+      dev: {
+        options: {
+          style: 'expanded'
+        },
+        src: ['public/sass/main.scss'],
+        dest: 'public/css/main.css'
+      }
+    },
+
   });
 
   // Load the installed tasks
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task
-  grunt.registerTask('default', ['']);
+  grunt.registerTask('default', ['watch']);
 
 };
