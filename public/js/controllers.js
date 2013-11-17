8e6f4b8c0edf4288bb1e33fc652c985f0101733f
Tim Schiller <timothy.schiller@gmail.com>
2013-11-16 18:04:15 -0800

    added factory and template flow

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,34 +1,48 @@
-var tillerControllers = angular.module('tillerControllers', []);
+var tillerControllers = angular.module('tillerControllers', [])
 
-tillerControllers.controller('EventListCtrl', ['$scope', '$routeParams',
-  function ($scope) {
-   // $http.get('phones/phones.json').success(function(data) {
-    //  $scope.phones = data;
-    //});ope.orderProp = 'age';
-    $scope.events = [
-        {'name': 'Nexus S',
-             'snippet': 'Fast just got faster with Nexus S.'},
-        {'name': 'Motorola XOOM™ with Wi-Fi',
-             'snippet': 'The Next, Next Generation tablet.'},
-        {'name': 'MOTOROLA XOOM™',
-             'snippet': 'The Next, Next Generation tablet.'}
-    ];
-  }
-]);
+  .factory("EventService", function(){
+    var formData = {};
 
-tillerControllers.controller('EventCreateCtrl', ['$scope', '$routeParams',
-  function($scope, $routeParams) {
-    $scope.saveEvent = function(event_data) {
-      console.log(event_data);
+    return {
+        getData: function () {
+            return formData;
+        },
+        setData: function (newFormData) {
+            formData = newFormData;
+        },
+        resetData: function () {
+            formData = {};
+        }
     };
-  }
-]);
+  })
 
-tillerControllers.controller('EventCtrl', ['$scope', '$routeParams', '$location',
-  function($scope, $routeParams, $location) {
-    $scope.saveEvent = function(event_data) {
-      console.log(this);
+  .controller('EventListCtrl', ['$scope', '$routeParams', function ($scope) {
+    //  $http.get('phones/phones.json').success(function(data) {
+    //    $scope.phones = data;
+    //  });ope.orderProp = 'age';
+    $scope.events = [{
+      'name': 'Nexus S',
+      'snippet': 'Fast just got faster with Nexus S.'
+    },
+    {
+      'name': 'Motorola XOOM™ with Wi-Fi',
+      'snippet': 'The Next, Next Generation tablet.'
+    },
+    {
+      'name': 'MOTOROLA XOOM™',
+      'snippet': 'The Next, Next Generation tablet.'
+    }];
+  }])
+
+  .controller('EventCreateCtrl', ['$scope', '$routeParams', 'EventService', function($scope, $routeParams, EventService) {
+    EventService.resetData();
+  }])
+
+  .controller('EventCtrl', ['$scope', '$routeParams', '$location', 'EventService', function($scope, $routeParams, $location, EventService) {
+    $scope.event = EventService.getData();
+    $scope.saveEvent = function() {
+      EventService.setData(this.event);
       $location.path('event/'+this.$id);
     };
-  }
-]);
+  }]);
+
