7664b37bb1f09a2a6d5de97ea0771e71d4145d1b
Tim Schiller <timothy.schiller@gmail.com>
2013-12-11 20:03:13 -0800

    some good ol refactoring

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,25 +1,23 @@
 var tillerControllers = angular.module('tillerControllers', [])
 
   .factory("EventService", function(){
-    var formData = {};
-
     return {
-        getData: function () {
-            return formData;
-        },
-        setData: function (newFormData) {
-            formData = newFormData;
-        },
-        resetData: function () {
-            formData = {};
-        }
+      eventName: '',
+      eventData: '',
+      eventColabs: [],
+      getData: function () {
+          return formData;
+      },
+      setData: function (newFormData) {
+          formData = newFormData;
+      },
+      resetData: function () {
+          formData = {};
+      }
     };
   })
 
   .controller('EventListCtrl', ['$scope', '$routeParams', function ($scope) {
-    //  $http.get('phones/phones.json').success(function(data) {
-    //    $scope.phones = data;
-    //  });ope.orderProp = 'age';
     $scope.events = [{
       'name': 'Nexus S',
       'snippet': 'Fast just got faster with Nexus S.'
@@ -34,15 +32,18 @@ var tillerControllers = angular.module('tillerControllers', [])
     }];
   }])
 
-  .controller('EventCreateCtrl', ['$scope', '$routeParams', 'EventService', function($scope, $routeParams, EventService) {
-    EventService.resetData();
+  .controller('EventCreateCtrl', ['$scope', '$location', '$routeParams', 'EventService', function($scope, $location, $routeParams, EventService) {
+    console.log(EventService);
+    $scope.eventName = EventService;
+//    $scope.eventDate = EventService.eventDate;
+//    $scope.eventColabs = EventService.eventColabs;
+    $scope.saveEvent = function() {
+      console.log(EventService);
+      //EventService.setData(this.event);
+      $location.path('event/'+this.$id);
+    };
   }])
 
   .controller('EventCtrl', ['$scope', '$routeParams', '$location', 'EventService', function($scope, $routeParams, $location, EventService) {
-    $scope.event = EventService.getData();
-    $scope.saveEvent = function() {
-      EventService.setData(this.event);
-      $location.path('event/'+this.$id);
-    };
   }]);
 
