38e2602b6c4620b90cb20fa186c4d7e40f85a2a0
Tim Schiller <timothy.schiller@gmail.com>
2014-03-07 12:34:29 -0800

    signup route

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,9 +8,15 @@ angular.module('tillerApp', [
 ])
   .config(function ($routeProvider) {
     $routeProvider
+
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
+      })
+
+      .when('/signup', {
+        templateUrl: 'views/signup.html',
+        controller: 'SignupCtrl'
       })
       .otherwise({
         redirectTo: '/'
--- app/scripts/controllers/signup.js
+++ app/scripts/controllers/signup.js
@@ -1,11 +1,10 @@
 'use strict';
 
 angular.module('tillerApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('SignupCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-    console.log('hi')
   });
